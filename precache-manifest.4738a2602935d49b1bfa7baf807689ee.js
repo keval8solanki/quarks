9b017bdda3b8e5a834c009c79821154e85249d75
Keval Solanki <57882119+keval8solanki@users.noreply.github.com>
2020-03-26 07:57:19 +0530

    Updates

---
+++ precache-manifest.4738a2602935d49b1bfa7baf807689ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "95f20d674480b92fb16b",
+    "url": "/quarks/static/css/main.88452dab.chunk.css"
+  },
+  {
+    "revision": "95f20d674480b92fb16b",
+    "url": "/quarks/static/js/main.528b2310.chunk.js"
+  },
+  {
+    "revision": "8a10f3e95664aeefd8b5",
+    "url": "/quarks/static/js/runtime~main.79b8a08b.js"
+  },
+  {
+    "revision": "a73769d3cc621c8b1d83",
+    "url": "/quarks/static/css/2.ca8b9da4.chunk.css"
+  },
+  {
+    "revision": "a73769d3cc621c8b1d83",
+    "url": "/quarks/static/js/2.d5ee7920.chunk.js"
+  },
+  {
+    "revision": "d463c225c57037a6730e15caadb32c8d",
+    "url": "/quarks/static/media/logout.d463c225.svg"
+  },
+  {
+    "revision": "ee951b2c881c9265cefac35bbe3e6200",
+    "url": "/quarks/index.html"
+  }
+];
